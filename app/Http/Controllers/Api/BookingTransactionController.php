8d894ba61a8d8633c87e0641e4651f80da81d62d
aldiyunan07 <aldiyunan07@gmail.com>
2025-01-30 12:29:26 +0700

    add integration whatsapp

--- app/Http/Controllers/Api/BookingTransactionController.php
+++ app/Http/Controllers/Api/BookingTransactionController.php
@@ -9,6 +9,8 @@ use App\Http\Resources\Api\ViewBookingResource;
 use App\Models\BookingTransaction;
 use App\Models\OfficeSpace;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Twilio\Rest\Client;
 
 class BookingTransactionController extends Controller
 {
@@ -29,6 +31,25 @@ class BookingTransactionController extends Controller
 
         // Mengirim notifikasi sms atau whatsapp dengan twilio
 
+        if (getenv("TWILIO_ACCOUND_SID") !== null && getenv("TWILIO_AUTH_TOKEN") !== null) {
+            $sId = getenv("TWILIO_ACCOUND_SID");
+            $token = getenv("TWILIO_AUTH_TOKEN");
+            $twilio = new Client($sId, $token);
+
+            $messageBody = "Hi {$bookingTransaction->name}, Terima kasih telah booking kantor di FirstOffice. \n";
+            $messageBody .= "Pesanan kantor {$bookingTransaction->officeSpace->name} Anda sedang kami proses dengan Booking TRX ID : {$bookingTransaction->booking_trx_id}.\n";
+            $messageBody .= "Kami akan mengonfirmasi kembali status pemesanan anda secepat mungkin.";
+
+            $message = $twilio->messages->create(
+                "whatsapp:+6285860256937",
+                [
+                    "body" => $messageBody,
+                    "from" => "whatsapp:".getenv("TWILIO_PHONE_NUMBER")
+                ]
+            );
+        }
+
+
         // Mengembalikan response berhasil
         $bookingTransaction->load('officeSpace');
         return new BookingTransactionResource($bookingTransaction);
@@ -43,7 +64,7 @@ class BookingTransactionController extends Controller
 
         $booking = BookingTransaction::where('booking_trx_id', $request->booking_trx_id)
             ->where('phone_number', $request->phone_number)
-            ->with(['officeSpace','officeSpace.city'])
+            ->with(['officeSpace', 'officeSpace.city'])
             ->first();
 
         if (!$booking) {
